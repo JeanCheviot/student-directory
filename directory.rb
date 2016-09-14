5f7393b6baccbf7b1ef7df521598563a6a4a6c4d
Joe Vincent <jeancheviot@icloud.com>
2016-09-14 11:02:42 +0000

    Directory now taking multiple information from user

--- directory.rb
+++ directory.rb
@@ -2,33 +2,45 @@ def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
     students = []
+    loop do
+    puts "What is the student\'s name?"
     name = gets.chomp
-    while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
+    if name.empty? 
+           break 
     end
+    puts "What cohort are you on?"
+    cohort = gets.chomp
+    puts "what are your hobbies?"
+    hobbies = gets.chomp
+    puts "What is your age?"
+    age = gets.chomp
+
+       students << {name: name, cohort: cohort, hobbies: hobbies, age: age}
+       puts "Now we have #{students.count} students"
+    end
+       
+
     return students
 end
 
 def print_header
-    puts "The students of Misunderstood Heartthrobs Academy"
-    puts "----------------"
+   puts "The students of Villains Academy"
+   puts "-------------"
 end
 
 def print(students)
-    students.each_with_index { |student, index| 
-    if student[:name].downcase.start_with?("g") && student[:name].length < 12
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" 
-    end
-    }
+   students.each_with_index do |student, index|
+     puts "#{index + 1}. #{student[:name]} on the #{student[:cohort]} Cohort. Age: #{student[:age]}. Hobbies: #{student[:hobbies]}."
+   end
 end
+
 
 def print_footer(students)
-    puts "Overall, we have #{students.count} great students"
+   puts "Overall, we have #{students.count} great students"
 end
 
-students = input_students
+
+students = input_students    
 print_header
 print(students)
 print_footer(students)
