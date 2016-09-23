b1f2ea8144eab21f6567266a7de910a774228d3e
Joe Vincent <jeancheviot@icloud.com>
2016-09-23 15:04:12 +0000

    Saving and Loading Data, plus taking arguments from the Command Line

--- directory.rb
+++ directory.rb
@@ -3,7 +3,7 @@
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end 
 
@@ -15,6 +15,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else 
@@ -23,26 +25,48 @@ def process(selection)
 end
 
 def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "9. Exit"
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
+    puts "9. Exit"
 end
 
 def show_students
-  print_header
-  print_students_list
-  print_footer
+    print_header
+    print_students_list
+    print_footer
 end
 
 def save_students
-  file = File.open("students.csv", "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+    student_data = [student[:name], student[:cohort], country[:country], age[:age]]
     csv_line = student_data.join(",")
     file.puts csv_line
+    end
+    file.close
+end
+
+def load_students(filename = "students.csv")
+    file = File.open(filename, "r")
+    file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym, country: country, age: age}
+    end
+    file.close
+end
+
+def try_load_students
+  filename = ARGV.first# first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
   end
-  file.close
 end
 
 def input_students
@@ -52,29 +76,29 @@ def input_students
     
     while true
         puts "What is the students name?"
-        name = gets.delete "\n"
+        name = STDIN.gets.chomp
     if name.empty?
         break 
     end
         puts "What cohort are you on?"
-        cohort = gets.chomp.capitalize
+        cohort = STDIN.gets.chomp.capitalize
     if cohort == ""
         cohort = "Undisclosed Cohort"
     end
         puts "What country are you from?"
-        country = gets.chomp.capitalize
+        country = STDIN.gets.chomp.capitalize
     if country == ""
         country = "Undisclosed Country"
     end
         puts "What is your age?"
-        age = gets.chomp
+        age = STDIN.gets.chomp
     if age == ""
         age = "Undisclosed Age"
     end
     
     
     puts "Is this information correct? (Y/N) \nName: #{name.capitalize} \nCohort: #{cohort} \nCountry: #{country} \nAge: #{age}"
-        answer = gets.chomp.upcase
+        answer = STDIN.gets.chomp.upcase
     if answer == 'N'
     break
     else
@@ -117,5 +141,8 @@ def print_footer
         end
 end
 
+
+
+try_load_students
 interactive_menu
 
